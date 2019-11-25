c81aed3b9f90fb0738a6f5cd59c6889199890b93
Dinesh Reddy Challa <A00210581@mycambrian.ca>
2019-11-25 15:44:21 -0500

    offlne lab

---
+++ js/sw.js
@@ -0,0 +1,26 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+workbox.preaching.precacheAndRoute([
+    '/index.html',
+    '/js/index.js',
+    '/img/add.jpg',
+    '/img/capture.jpg'
+])
+workbox.routing.registerRoute(
+    /\.htm(l?)$/,
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'html-cache',
+    })
+);
+workbox.routing.registerRoute(
+    /\.js$/,
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'js-cache',
+    })
+
+);
+workbox.routing.registerRoute(
+    /\.css$/,
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'css-cache'
+    })
+);
